8d981abb666d5e4890e2b2fac7f672b4b4fe56a7
Kai Vandivier <kpvandivier@gmail.com>
2021-04-21 17:34:52 +0200

    feat: add test links and requests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // This function should be provided by the API
+    function recordRequests(networkAction, recordingTimeoutDelay) {
+        console.log('[App] Sending "Start recording" message')
+        navigator.serviceWorker.controller?.postMessage({
+            type: 'START_RECORDING',
+            recordingTimeoutDelay,
+            recordedSectionId: '1234',
+        })
+        return networkAction()
+    }
+
+    async function cascadingRequests() {
+        console.log('[App] Cascading requests started')
+
+        const unsplashBaseUrl = 'https://source.unsplash.com/featured/?'
+        const keywords = ['nature1', 'water2', 'cat3', 'bird4']
+
+        const res1 = await fetch(unsplashBaseUrl + keywords[0])
+        const res2 = await fetch(unsplashBaseUrl + keywords[1])
+        console.log('[App] responses: (now waiting 500ms)', {
+            res1,
+            res2,
+        })
+
+        await new Promise((resolve) => {
+            setTimeout(resolve, 500)
+        })
+        console.log(
+            '[App] Finished 500 ms wait between requests - fetching again'
+        )
+
+        const res3 = await fetch(unsplashBaseUrl + keywords[2])
+        console.log('[App] Res 3: (now waiting 1.5s)', { res3 })
+
+        await new Promise((resolve) => {
+            setTimeout(resolve, 1500)
+        })
+        console.log('[App] Finished 1.5s wait - fetching again')
+
+        const res4 = await fetch(unsplashBaseUrl + keywords[3])
+        console.log('[App] Finished final fetch in cascadingRequests:', {
+            res4,
+        })
+        return { res1, res2, res3, res4 }
+    }
+
+    async function onClick() {
+        const recordingTimeoutDelay = 2000
+        console.log('[App] Triggering cascading requests and recording', {
+            recordingTimeoutDelay,
+        })
+        const res = await recordRequests(
+            cascadingRequests,
+            recordingTimeoutDelay
+        )
+        console.log('[App] result of recordResults callback:', { res })
+    }
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div className="button-container">
+                    <button onClick={onClick}>
+                        {'Trigger cascading requests & record'}
+                    </button>
+                    <p>Links to static resources:</p>
+                    <a href="./cats/green-cat-png.png" className="App-link">
+                        Green cat
+                    </a>
+                    <a href="./cats/fluffy-cat-png.png" className="App-link">
+                        Fluffy cat
+                    </a>
+                    <a
+                        href="./cats/transparent-orange-white-cat-png.png"
+                        className="App-link"
+                    >
+                        Orange cat
+                    </a>
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
